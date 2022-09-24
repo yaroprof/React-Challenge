3fa944e704e102e25ae964f274459be8ab41324e
yaroprof <crosskyiv@gmail.com>
2022-09-24 14:28:22 +0300

    react-forms complete

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [objData, setObjData] = useState({});
+  const getInputs = (value, name) => {
+    const data = { [name]: value };
+    setObjData({ ...objData, ...data });
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(objData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={submit}>
+      <div className="form">
+        <input
+          placeholder="Write your name"
+          name="name"
+          type="text"
+          onChange={(event) => getInputs(event.target.value, event.target.name)}
+        />
+
+        <input
+          placeholder="Write your age"
+          name="age"
+          type="number"
+          onChange={(event) => getInputs(event.target.value, event.target.name)}
+        />
+
+        <input
+          placeholder="Write your hobbies"
+          name="date"
+          type="text"
+          onChange={(event) => getInputs(event.target.value, event.target.name)}
+        />
+
+        <input
+          placeholder="Write a Date"
+          name="date"
+          type="date"
+          onChange={(event) => getInputs(event.target.value, event.target.name)}
+        />
+
+        <input
+          placeholder="Input date"
+          name="date"
+          type="file"
+          onChange={(event) => getInputs(event.target.value, event.target.name)}
+        />
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </div>
+    </form>
   );
 }
 
